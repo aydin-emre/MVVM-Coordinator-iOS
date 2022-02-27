2571e64a65ca81abcbd5ed0ec44297d0caf24ec3
Emre Huawei <ea@ea.tc>
2022-02-27 12:20:25 +0300

    Project created..

---
+++ MVVM Coordinator Example/MVVM Coordinator Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Coordinator Example
+//
+//  Created by Emre on 27.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
